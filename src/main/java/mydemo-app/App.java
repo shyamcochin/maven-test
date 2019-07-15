b7cf20317f8acefe9253b8f465917c930550c6a4
Syam Kumar <shyamcochin@gmail.com>
2019-07-15 20:34:55 +0530

    maven 1st commit

---
+++ src/main/java/mydemo-app/App.java
@@ -0,0 +1,13 @@
+package mydemo-app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

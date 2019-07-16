fd7fe7b5b471a65486947d8e73bdc728df4fa6d1
Syam Kumar PB <syam.kumar@rubyseven.net>
2019-07-16 14:10:37 +0530

    updated example.sh

--- example.sh
+++ example.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo yum update
-sudo yum install httpd
+## https://devopscube.com/packer-tutorial-for-beginners/
+sudo yum -y update
+sudo yum install -y httpd
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
-sudo echo "Hello World" /var/www/html/index.html
+sudo echo "Hello World" > /var/www/html/index.html
